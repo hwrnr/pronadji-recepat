9517c439e4ddb6594251caa865cd88edd8e53b2f
hwrnr <hawerner.hawerner@gmail.com>
2024-06-01 18:10:57 +0200

    Fix random bugs

--- FoodCNN/main.py
+++ FoodCNN/main.py
@@ -14,16 +14,16 @@ app = Flask(__name__)
 CORS(app)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000 # 16 MB
 
-with open('FoodCNN/labels', 'r') as file:
+with open('labels', 'r') as file:
     labels = json.load(file)
 
 
-model = keras.saving.load_model("FoodCNN/Model/bestmodel-2")
+model = keras.saving.load_model("Model/bestmodel-2")
 
 def recognize_image(image) -> str:
     # Convert image to pillow image
     image = Image.open(BytesIO(base64.b64decode(image)))
-    image.reshape((224, 224))
+    image = image.resize((224, 224))
     img_array = krsimg.img_to_array(image)
     img_array = np.expand_dims(img_array, axis=0)
 
@@ -35,7 +35,7 @@ def recognize_image(image) -> str:
     for i in top_classes:
         classes.append(labels[i])
 
-    return classes[0]
+    return classes[0].replace("_", " ")
 
 @app.route('/recognizeImage', methods=['POST'])
 def recognize_image_api():
