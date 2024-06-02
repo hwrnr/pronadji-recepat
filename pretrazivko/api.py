dc4d87d0302c267a92e61882913495428d8fe2c0
hwrnr <hawerner.hawerner@gmail.com>
2024-06-02 02:23:42 +0200

    Added get recipe placeholder

--- pretrazivko/api.py
+++ pretrazivko/api.py
@@ -6,6 +6,7 @@ CORS(app)
 
 @app.route('/search/<query>')
 def search(query):
+    # TODO: Implement search functionality
     return jsonify([
         {
             "id": 1,
@@ -18,6 +19,15 @@ def search(query):
             "description": 'The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951.',
             },
         ])
+
+@app.route('/recipe/<id>')
+def recipe(id):
+    # TODO: Implement get recipe functionality
+    return jsonify({
+        "id": id,
+        "title": 'The Great Gatsby',
+        "description": 'The Great Gatsby is a novel written by American author F. Scott Fitzgerald that follows a cast of characters living in the fictional towns of West Egg and East Egg on prosperous Long Island in the summer of 1922.',
+        })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
